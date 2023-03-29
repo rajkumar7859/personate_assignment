47af5a3dc83731739b1bc04ebe2530218360f344
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-03-30 01:17:57 +0530

    all check before deploy

--- personate_assignment/src/component/UploadFile.jsx
+++ personate_assignment/src/component/UploadFile.jsx
@@ -1,96 +1,131 @@
-import React, { useEffect,useState } from "react";
+import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import AWS from "aws-sdk";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-const S3_BUCKET ='personateaiassign';
-const REGION ='ap-south-1';
-
+const S3_BUCKET =process.env.REACT_APP_AWS_BUCKET_NAME;
+const REGION = "ap-south-1";
 
 AWS.config.update({
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
   secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
   signatureVersion: "v4",
-})
+});
 
 const myBucket = new AWS.S3({
-    params: { Bucket: S3_BUCKET},
-    region: REGION,
-})
+  params: { Bucket: S3_BUCKET },
+  region: REGION,
+});
 
-const UploadFile=()=>{
-  // const [uploadProgress, setUploadProgress] = useState(0);
-  const [progress , setProgress] = useState(0);
+const UploadFile = () => {
+  const [progress, setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState(null);
-    const {
-      getRootProps,
-      getInputProps,
-      isFocused,
-      isDragAccept,
-      isDragReject,
-      acceptedFiles,
-    } = useDropzone({ accept: { "video/mp4": [] }, multiple: false });
-  
-    useEffect(() => {
-      if (!acceptedFiles.length) return;
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    accept: { "video/mp4": [] },
+    multiple: false,
+    onDrop: (acceptedFiles, rejectedFiles, event) => {
+      event.preventDefault();
+      if (rejectedFiles.length) {
+        return;
+      }
       setSelectedFile(acceptedFiles[0]);
-    }, [acceptedFiles]);
-    
-    const handleUpload = async () => {
+    },
+  });
+
+
+
+  const handleUpload = async () => {
+    try {
+      
       if (!selectedFile) {
         return;
       }
       const params = {
         Body: selectedFile,
         Bucket: S3_BUCKET,
-        Key: selectedFile.name
+        Key: selectedFile.name,
       };
-myBucket.putObject(params)
-        .on('httpUploadProgress', (evt) => {
-            setProgress(Math.round((evt.loaded / evt.total) * 100))
+      myBucket
+        .putObject(params)
+        .on("httpUploadProgress", (evt) => {
+          setProgress(Math.round((evt.loaded / evt.total) * 100));
         })
         .send((err) => {
-            if (err) console.log("errpr",err)
-        })
-
-      }
+          if (err) console.log("error", err);
+        });
+    } catch (error) {
+      console.log("Upload error " ,error);
+    }
+  };
 
   return (
-    <section className="">
-      <div {...getRootProps()}>
+    <section className="flex flex-col items-center justify-center mt-10">
+      <div
+        {...getRootProps({
+          className: `cursor-pointer flex flex-col items-center justify-center w-80 h-80 border-4 border-dashed rounded-lg transition-all ${
+            isDragAccept ? "border-green-500" : "border-gray-400"
+          }`,
+        })}
+      >
         <input {...getInputProps()} />
-        <h1>You can upload video here</h1>
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        {isDragAccept && (
+          <p className="text-green-500 font-bold text-lg">
+            Drop it like it's hot 🔥
+          </p>
+        )}
+        {isDragReject && (
+          <p className="text-red-500 font-bold text-lg">
+            Nope, not that file type 👎
+          </p>
+        )}
+        {
+          !isFocused?(<p className="text-gray-500 font-medium text-lg">
+          Drag 'n' drop a video file here, or click to select a file
+        </p>): (<p className="text-gray-500 font-medium text-lg">
+           Click to Drop it like it's hot 🔥
+          </p>)
+        }
       </div>
       {selectedFile && progress === 0 && (
-        <div className="">
-          <p>File {selectedFile.name} selected for upload</p>
+        <div className="mt-5">
+          <p className="text-gray-500 font-medium text-lg">
+            File {selectedFile.name} selected for upload
+          </p>
         </div>
       )}
-      <div style={{ marginTop: "10px" }}>
+
+      <div className="mt-5">
         {progress !== 0 && (
-          <div className="" role="">
-            <p>File {selectedFile.name} is uploading...</p>
+          <div className="w-80">
+            <p className="text-gray-500 font-medium text-lg">
+               {progress!==100? `File {selectedFile.name} is uploading...`:"Uploading completed"} 
+            </p>
             <div className="mt-2">
-            <ProgressBar
+              <ProgressBar
                 completed={progress}
                 label={`${progress}% completed`}
                 bgColor="#19a7ce"
               />
-              ;
             </div>
           </div>
         )}
-        <div className="">
-          <div className="">
-            <button className="" onClick={handleUpload}>
-              Upload video
-            </button>
-          </div>
+        <div className="flex justify-center mt-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={handleUpload}
+          >
+            Upload video
+          </button>
         </div>
-      </div> 
+      </div>
     </section>
   );
-      }
+};
 
-export default UploadFile
+export default UploadFile;
